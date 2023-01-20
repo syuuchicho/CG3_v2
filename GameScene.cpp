c7560541b6217c5dcc50357c4d0b64f584903365
syuuchicho <k021g1478@g.neec.ac.jp>
2023-01-20 10:50:31 +0900

    平均化

--- GameScene.cpp
+++ GameScene.cpp
@@ -21,7 +21,6 @@ GameScene::~GameScene()
 	delete modelFighter;
 	delete modelSphere;
 	delete camera;
-	delete light;
 }
 
 void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
@@ -41,14 +40,14 @@ void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
 	// テクスチャ読み込み
 	Sprite::LoadTexture(1, L"Resources/background.png");
 
-	// カメラ生成
+    // カメラ生成
 	camera = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight, input);
 
 	// カメラ注視点をセット
-	camera->SetTarget({ 0, 1, 0 });
+	camera->SetTarget({0, 1, 0});
 	camera->SetDistance(3.0f);
 
-	// 3Dオブジェクトにカメラをセット
+    // 3Dオブジェクトにカメラをセット
 	Object3d::SetCamera(camera);
 
 	// 背景スプライト生成
@@ -59,91 +58,37 @@ void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
 	objFighter = Object3d::Create();
 	objSphere = Object3d::Create();
 
-	//ライト生成
-	light = Light::Create();
-	//ライト色を設定
-	light->SetLightColor({ 1,1,1 });
-	//3Dオブジェクトにライトをセット
-	Object3d::SetLight(light);
-
 	// テクスチャ2番に読み込み
 	Sprite::LoadTexture(2, L"Resources/texture.png");
 
 	modelSkydome = Model::CreateFromOBJ("skydome");
 	modelGround = Model::CreateFromOBJ("ground");
-	modelFighter = Model::CreateFromOBJ("chr_sword");
-	modelSphere = Model::CreateFromOBJ("sphere", true);
+	//modelFighter = Model::CreateFromOBJ("chr_sword");
+	modelFighter = Model::CreateFromOBJ("sphere");
+	modelSphere = Model::CreateFromOBJ("sphere",true);
 
 	objSkydome->SetModel(modelSkydome);
 	objGround->SetModel(modelGround);
 	objFighter->SetModel(modelFighter);
 	objSphere->SetModel(modelSphere);
-	objFighter->SetPosition({ +1,0,0 });
-	objSphere->SetPosition({ -1,1,0 });
+	//objFighter->SetPosition({ +1,0,0 });
+	objFighter->SetPosition({ -1,1,0 });
+	objSphere->SetPosition({ +1,1,0 });
 
 }
 
 void GameScene::Update()
 {
 	camera->Update();
-	light->Update();
 
 	objSkydome->Update();
 	objGround->Update();
 	objFighter->Update();
 	objSphere->Update();
 
-#pragma region オブジェクトの回転
-	XMFLOAT3 rot = objSphere->GetRotation();
-	rot.y += 1.0f;
-	objSphere->SetRotation(rot);
-	objFighter->SetRotation(rot);
-#pragma endregion
-
-	//光線方向初期値			   上奥
-	static XMVECTOR lightDir = { 0,1,5,0 };
-
-	if (input->PushKey(DIK_W))
-	{
-		lightDir.m128_f32[1] += 1.0f;
-	}
-	else if (input->PushKey(DIK_S))
-	{
-		lightDir.m128_f32[1] -= 1.0f;
-	}
-	if (input->PushKey(DIK_D))
-	{
-		lightDir.m128_f32[0] += 1.0f;
-	}
-	else if (input->PushKey(DIK_A))
-	{
-		lightDir.m128_f32[0] -= 1.0f;
-	}
-
-	light->SetLightDir(lightDir);
-
-	std::ostringstream debugstr;
-	debugstr << "lightDirFactor("
-		<< std::fixed << std::setprecision(2)
-		<< lightDir.m128_f32[0] << ","
-		<< lightDir.m128_f32[1] << ","
-		<< lightDir.m128_f32[2] << ")";
-	debugText.Print(debugstr.str(), 50, 50, 1.0f);
-	debugstr.str("");
-	debugstr.clear();
-
-	const XMFLOAT3& cameraPos = camera->GetEye();
-	debugstr << "cameraPos("
-		<< std::fixed << std::setprecision(2)
-		<< cameraPos.x << ","
-		<< cameraPos.y << ","
-		<< cameraPos.z << ")";
-	debugText.Print(debugstr.str(), 50, 70, 1.0f);
-
-
-	/*debugText.Print("AD: move camera LeftRight", 50, 50, 1.0f);
+	debugText.Print("AD: move camera LeftRight", 50, 50, 1.0f);
 	debugText.Print("WS: move camera UpDown", 50, 70, 1.0f);
-	debugText.Print("ARROW: move camera FrontBack", 50, 90, 1.0f);*/
+	debugText.Print("ARROW: move camera FrontBack", 50, 90, 1.0f);
 }
 
 void GameScene::Draw()
@@ -172,7 +117,7 @@ void GameScene::Draw()
 	Object3d::PreDraw(cmdList);
 
 	// 3Dオブクジェクトの描画
-	//objSkydome->Draw();
+	objSkydome->Draw();
 	objGround->Draw();
 	objFighter->Draw();
 	objSphere->Draw();
--- GameScene.h
+++ GameScene.h
@@ -6,7 +6,6 @@
 #include "Input.h"
 #include "Object3d.h"
 #include "Sprite.h"
-#include "Light.h"
 #include <DirectXMath.h>
 
 /// <summary>
@@ -61,7 +60,6 @@ class GameScene {
 	/// ゲームシーン用
 	/// </summary>
 	Sprite* spriteBG = nullptr;
-	Light* light = nullptr;
 
 	Model* modelSkydome = nullptr;
 	Model* modelGround = nullptr;
